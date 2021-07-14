d90a48387de22f6741ed86deef580bb812654ef0
tsborissov <68992187+tsborissov@users.noreply.github.com>
2021-07-14 19:10:47 +0300

    Implemented adding new Category

---
+++ ITAssetManager.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using ITAssetManager.Data;
+using ITAssetManager.Data.Models;
+using ITAssetManager.Web.Models.Categories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ITAssetManager.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ItAssetManagerDbContext data;
+
+        public CategoriesController(ItAssetManagerDbContext data)
+        {
+            this.data = data;
+        }
+
+        [Authorize]
+        public IActionResult Add() => View();
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Add(CategoryAddFormModel categoryModel)
+        {
+            if (this.data.Categories.Any(c => c.Name == categoryModel.Name))
+            {
+                this.ModelState.AddModelError(nameof(categoryModel.Name), "Category already exists!");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return View(categoryModel);
+            }
+
+            var category = new Category 
+            {
+                Name = categoryModel.Name
+            };
+
+            this.data.Categories.Add(category);
+            this.data.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ITAssetManager.Web/Models/Categories/CategoryAddFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static ITAssetManager.Data.DataConstants;
+
+namespace ITAssetManager.Web.Models.Categories
+{
+    public class CategoryAddFormModel
+    {
+        [Required]
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength)]
+        public string Name { get; init; }
+    }
+}
