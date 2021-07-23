17c70e2cd5618e1a27b5c881edc116b9940259b0
tsborissov <68992187+tsborissov@users.noreply.github.com>
2021-07-23 16:39:41 +0300

    Introduced API for querying all brands.

---
+++ ITAssetManager.Web/Controllers/Api/BrandsApiController.cs
@@ -0,0 +1,24 @@
+﻿using ITAssetManager.Web.Models.Api.Brands;
+using ITAssetManager.Web.Services.Brands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITAssetManager.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("api/brands")]
+    public class BrandsApiController : ControllerBase
+    {
+        private readonly IBrandService brandService;
+
+        public BrandsApiController(IBrandService brandService) 
+            => this.brandService = brandService;
+
+        [HttpGet]
+        public BrandQueryServiceModel All([FromQuery] AllBrandsApiRequestModel query)
+            => this.brandService.All(
+                query.SearchString,
+                query.SortOrder,
+                query.CurrentPage,
+                query.BrandsPerPage);
+    }
+}
---
+++ ITAssetManager.Web/Models/Api/Brands/AllBrandsApiRequestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ITAssetManager.Web.Models.Api.Brands
+{
+    public class AllBrandsApiRequestModel
+    {
+        public string SearchString { get; init; }
+
+        public string SortOrder { get; init; }
+
+        public int CurrentPage { get; init; }
+
+        public int BrandsPerPage { get; init; } = 10;
+
+        public bool HasPreviousPage { get; init; }
+
+        public bool HasNextPage { get; init; }
+    }
+}
